4c5ce2c4d1cc6ac3c7ad55eac7842f4fac0b78ea
huangyulie <1279072972@qq.com>
2022-05-20 17:19:47 +0800

    1.21

--- blog/src/containers/bar/bar.jsx
+++ blog/src/containers/bar/bar.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import ReacEcharts from 'echarts-for-react';
 
 export default function Bar() {
+  const getOption = ()=>{
+    return {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    }
+  }
   return (
-    <div>B</div>
+    <div>
+      <ReacEcharts option={getOption()}></ReacEcharts>
+    </div>
   )
 }
--- blog/src/containers/pie/pie.jsx
+++ blog/src/containers/pie/pie.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import ReacEcharts from 'echarts-for-react';
 
 export default function Pie() {
+  const getOption = ()=>{
+    return {
+      series: [
+        {
+          type: 'pie',
+          data: [
+            {
+              value: 100,
+              name: 'A'
+            },
+            {
+              value: 200,
+              name: 'B'
+            },
+            {
+              value: 300,
+              name: 'C'
+            },
+            {
+              value: 400,
+              name: 'D'
+            },
+            {
+              value: 500,
+              name: 'E'
+            }
+          ],
+          roseType: 'area'
+        }
+      ]
+    };
+  }
   return (
-    <div>pie</div>
+    <div>
+       <ReacEcharts option={getOption()}></ReacEcharts>
+    </div>
   )
 }
